24f7dada2c24f295428b1aca297f45fd6bc6bad5
Barkou abderrahmane <Iman555bpe147@gmail.com>
2024-08-27 15:32:17 +0100

    Added task/tab deletion

--- src/app/Components/Task.js
+++ src/app/Components/Task.js
@@ -4,12 +4,61 @@ import { useTheme } from "./ThemeContext"
 import { findActiveProject } from "./NewButton";
 import "./tasksCSS.css"
 import { useUser } from "./UserContext";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { useLongPress } from "use-long-press";
 
 const Task = ({name,isCompleted,update,index,tabIndex}) => {
   
   const [currentTheme,] = useTheme()
   const [user,setUser] = useUser()
   const activeIndex = findActiveProject(user)
+
+  const [deleteState,setDeleteState] = useState(false)
+  const showDelete = () => {
+    setDeleteState(true)
+  }
+  const bindFunc = useLongPress(showDelete,{threshold:700})
+
+  const cancelDeleteTask = () => {
+    setDeleteState(false)
+  }
+
+  const deleteTask = () => {
+    let userCopy = user
+    userCopy.projectList[activeIndex].tabList[tabIndex].taskArray.splice(index,1)
+    setUser(userCopy)
+    update()
+  }
+
+  const ConfirmDelete = () => {
+    return (
+      <motion.div
+        className="deletion-container"
+        animate={{
+          opacity: 1,
+          scale: 1,
+        }}
+        initial={{
+          opacity: 0,
+          scale: 0.7,
+        }}
+        transition={{
+          delay: 0.2,
+          duration: 0.2,
+        }}
+      >
+        <div className="deletion-container-upper">
+          <p className="deletion-text"> Are you sure u want to delete:</p>
+          <p className="deletion-text-accent">"{name}"</p>
+        </div>
+        <div className="deletion-container-lower">
+          <div className="deletion-button"onClick={deleteTask}>YES</div>
+          <div className="deletion-button"onClick={cancelDeleteTask}>NO</div>
+        </div>
+      </motion.div>
+    )
+  }
 
   const strokeColor = isCompleted ? currentTheme.accent : currentTheme.secondary
   
@@ -22,12 +71,33 @@ const Task = ({name,isCompleted,update,index,tabIndex}) => {
   }
 
   return (
-    <div className="Task-Container">
-      <span>{name}</span>
-      <div onClick={completeTask} style={{cursor: "pointer"}}>
-        <TaskElipse stroke={strokeColor} fill={currentTheme.primary} />
-      </div>
-    </div>
+    <motion.div 
+      className="Task-Container"
+      {...bindFunc()}
+
+      animate={deleteState ? "open":"close"}
+      variants={{
+        open: {
+          padding: "15px 0px",
+        },
+        close: {
+        }
+      }}
+
+      transition={{
+        type: "spring",
+        duration: 0.6,
+      }}
+    >
+      {deleteState ? (<ConfirmDelete />) : (
+        <>
+          <span>{name}</span>
+          <div onClick={completeTask} style={{cursor: "pointer"}}>
+            <TaskElipse stroke={strokeColor} fill={currentTheme.primary} />
+          </div>
+        </>
+      )}
+    </motion.div>
   )
 }
 
--- src/app/Components/TaskTab.js
+++ src/app/Components/TaskTab.js
@@ -1,15 +1,86 @@
 "use client"
 import "./tasksCSS.css";
 import Task from "./Task";
-
+import { useState } from "react";
+import { useLongPress } from "use-long-press";
+import { motion } from "framer-motion";
+import { useUser } from "./UserContext";
+import { findActiveProject } from "./NewButton";
 
 const TaskTab = ({name, TaskArray, update, index}) => {
 
+  const [deleteState,setDeleteState] = useState(false)
+  const [user,setUser] = useUser()
+  const activeIndex = findActiveProject(user)
+  const showDelete = () => {
+    setDeleteState(true)
+  }
+  const bindFunc = useLongPress(showDelete,{threshold:700})
+  
+
+  const deleteTab = () => {
+    let userCopy = user;
+    userCopy.projectList[activeIndex].tabList.splice(index,1)
+    setUser(userCopy)
+    update()
+  }
+
+  const cancelDeleteTab = () => {
+    setDeleteState(false)
+  }
+
+  const ConfirmDelete = () => {
+    return (
+      <motion.div
+        className="deletion-container"
+        animate={{
+          opacity: 1,
+          scale: 1,
+        }}
+        initial={{
+          opacity: 0,
+          scale: 0.7,
+        }}
+        transition={{
+          delay: 0.4,
+          duration: 0.2,
+        }}
+      >
+        <div className="deletion-container-upper">
+          <p className="deletion-text"> Are you sure u want to delete:</p>
+          <p className="deletion-text-accent">"{name}"</p>
+        </div>
+        <div className="deletion-container-lower">
+          <div className="deletion-button"onClick={deleteTab}>YES</div>
+          <div className="deletion-button"onClick={cancelDeleteTab}>NO</div>
+        </div>
+      </motion.div>
+    )
+  }
+  
+
   return (
     <div className="Tab-container">
-      <div className="Tab-Header">
-        {name}
-      </div>
+      <motion.div 
+        className="Tab-Header"
+        {...bindFunc()}
+
+        animate={deleteState ? "open":"close"}
+        variants={{
+          open: {
+            padding: "15px 0px",
+          },
+          close: {
+          }
+        }}
+
+        transition={{
+          type: "spring",
+          duration: 0.6,
+        }}
+      >
+        {deleteState ? [<ConfirmDelete />]:[<span>{name}</span>]}
+      </motion.div>
       <div className="Tab-Body">       
         {TaskArray.map((t,i) => {
           return (          
